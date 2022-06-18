c0a295beffa8744ae2953b610326a3bd3c8a78ef
Orel <ryiseld@gmail.com>
2022-06-18 01:59:43 -0700

    Remove unnecessary using

--- main.cpp
+++ main.cpp
@@ -4,12 +4,10 @@
 
 #include "window_manager.hpp"
 
-using std::unique_ptr;
-
 int main(int argc, char** argv) {
     google::InitGoogleLogging(argv[0]);
 
-    unique_ptr<WindowManager> window_manager(WindowManager::Create());
+    std::unique_ptr<WindowManager> window_manager(WindowManager::Create());
     if (!window_manager) {
         LOG(ERROR) << "Failed to initialize window manager";
         return EXIT_FAILURE;
